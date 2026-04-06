e18d6950bcf889accfa89a7f88e646c5bc8620ab
Jacob <jacobsponsel4564@gmail.com>
2026-04-06 12:44:43 -0700

    minor changes: pacstrap, small core structure suggestions

--- Step1.sh
+++ Step1.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+# pull all variables in from file here:
+## Packages
+
+## Other vars
+
+
 # check bootmode
+bootmode=$(cat /sys/firmware/efi/fw_platform_size)
+if [[$bootmode != 64 ]]; then
+	echo Bootmode does not equal 64 exiting script.
+	exit
+else
+	echo Bootmode check passed!
+fi
 
 # set time via timedatectl
 timedatectl
 
+
+## potentialy add in mirror sorting here for faster install!!!!
+
+
 # check CPU architecture and prepare for adding required microcode to pacstrap command
 
+
 # run pacstrap (just base packages for now)
+pacstrap -K /mnt $basePackages
 
 # genfstab
 genfstab -U /mnt >> /mnt/etc/fstab
