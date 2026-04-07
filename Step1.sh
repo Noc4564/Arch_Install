9c7bfab3a0861b94387209be0a86e03c4fc96fe1
Jacob <jacobsponsel4564@gmail.com>
2026-04-06 18:46:49 -0700

    started on getting pacstrap sorted out including hopefully functioning CPU vendor detection

--- Step1.sh
+++ Step1.sh
@@ -2,9 +2,9 @@
 
 # pull all variables in from file here:
 ## Packages
-
-## Other vars
-
+source basePackages.txt
+## Vars
+source config.txt
 
 # check bootmode
 bootmode=$(cat /sys/firmware/efi/fw_platform_size)
@@ -23,10 +23,23 @@ timedatectl
 
 
 # check CPU architecture and prepare for adding required microcode to pacstrap command
-
+## get vendor ID and cut string to easy to parse for what vendor is in use
+cpuVendorPreprocess=$(lscpu | grep Vendor)
+cpuVendor="${cpuVendorPreprocess##* }"
+## determine vendor for use in pacstrap
+if [[ "$cpuVendor" == "GenuineIntel" ]]; then
+	echo Vendor is: "Intel"
+	pacVendor="intel-ucode"
+elif [[ "$cpuVendor" == "GenuineAMD" ]]; then
+	echo Vendor is: "AMD"
+	pacVendor="amd-ucode"
+else
+	echo Cannot determine CPU vendor!
+	exit
+fi
 
 # run pacstrap (just base packages for now)
-pacstrap -K /mnt $basePackages
+pacstrap -K /mnt "${basePackages[@]}"
 
 # genfstab
 genfstab -U /mnt >> /mnt/etc/fstab
