d5e813f29fff59272ba1b4c2b046878ed21fc81d
Jacob <jacobsponsel4564@gmail.com>
2026-04-07 13:03:15 -0700

    added Step2.sh and significant progress towards getting that functional as well as getting config.txt exisiting with a few default values included

---
+++ Step2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# pull in all variables
+source config.txt
+
+# set timezone
+ln -sf /usr/share/zoneinfo/"$timezone" /etc/localtime
+
+# set hwclock
+hwclock --systoch
+
+# locale uncomment section and locale-gen
+## uncomment locale
+
+## gen locale
+locale-gen
+
+# hostname
+echo "$hostname" >> /etc/hostname:create
+
+# probably not actually needed but run anyways it is for the systemd initial RAM file system
+mkinitcpio -P
+
+# set root password
+echo "$rootPassword" | sudo chpasswd
+
+# create new user
+useradd -m -G wheel "$userName"
+
+# make users in wheel group a sudoer
+echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
+
+# enable system services
+
+# boot stuff
+#
+
